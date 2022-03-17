ba1ea182bc1b5638562b768458c6636bc0583352
Miguel Palhas <mpalhas@gmail.com>
2022-03-17 13:34:34 +0000

    code review

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,10 +1,13 @@
 import { task } from "hardhat/config";
+import { NetworksUserConfig } from "hardhat/types";
 
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
+
+const { MANDALA_MNEMONIC } = process.env;
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -13,6 +16,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+let networks: NetworksUserConfig = {};
+
+networks["hardhat"] = {
+  blockGasLimit: 3000000000,
+};
+
+if (!!MANDALA_MNEMONIC) {
+  networks["mandala"] = {
+    url: "https://tc7-eth.aca-dev.network",
+    accounts: {
+      mnemonic: MANDALA_MNEMONIC,
+    },
+    chainId: 595,
+  };
+}
 
 const config = {
   solidity: {
@@ -24,6 +43,7 @@ const config = {
       },
     },
   },
+  networks,
   namedAccounts: {
     deployer: 0,
   },
--- packages/contracts/test/contracts/RisingTide/RisingTide.gas.ts
+++ packages/contracts/test/contracts/RisingTide/RisingTide.gas.ts
@@ -1,5 +1,6 @@
 import type { ContractFactory } from "ethers";
 import type { TestRisingTideWithStaticAmounts } from "../../../src/types";
+import { calcEthereumTransactionParams } from "@acala-network/eth-providers";
 
 import { ethers } from "hardhat";
 
@@ -11,12 +12,28 @@ import { expectCapValidation } from "./helpers";
  */
 if (process.env.RISING_TIDE_GAS_ESTIMATES) {
   describe("RisingTide", () => {
+    const txFeePerGas = "199999946752";
+    const storageByteDeposit = "100000000000000";
     let WithStaticAmounts: ContractFactory;
+
+    let ethParams: any;
 
     beforeEach(async () => {
       WithStaticAmounts = await ethers.getContractFactory(
         "TestRisingTideWithStaticAmounts"
       );
+
+      const blockNumber = await ethers.provider.getBlockNumber();
+
+      const { txGasPrice, txGasLimit } = calcEthereumTransactionParams({
+        gasLimit: "2100001",
+        validUntil: (blockNumber + 100).toString(),
+        storageLimit: "64001",
+        txFeePerGas,
+        storageByteDeposit,
+      });
+
+      ethParams = { gasPrice: txGasPrice, gasLimit: txGasLimit };
     });
 
     describe("gas estimates", function () {
@@ -25,13 +42,11 @@ if (process.env.RISING_TIDE_GAS_ESTIMATES) {
       const results: Record<string, string> = {};
       const total = 1000000;
 
-      [2500, 5000, 10000, 20000].forEach((n: number) => {
+      [2500].forEach((n: number) => {
         it(`${n} investors`, async function () {
-          const contract = (await WithStaticAmounts.deploy(
-            n,
-            total,
-            total
-          )) as TestRisingTideWithStaticAmounts;
+          const contract = (await WithStaticAmounts.deploy(n, total, total, {
+            ...ethParams,
+          })) as TestRisingTideWithStaticAmounts;
           await contract.deployed();
 
           const cap = Math.floor(total / n);

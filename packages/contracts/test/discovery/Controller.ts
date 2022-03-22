a9bf94c944de8c42e1f829a1a7330dc70aa0bf35
Davide Silva <davide@finiam.com>
2022-03-22 15:08:50 +0000

    Controller contract Project registration
    Why:
    * Initial implementation of the `registerProject` function for the
      Controller contract
    
    How:
    * Adding a `registerProject` function that allows adding a new
      Project to the `projects`
    * Adding a `RegisterProject` event and a `ProjectStatus` enum
    * Adding `lastProjectId` and `lastBatchId` to keep track of the last
      generated Project and Batch, respectively

--- packages/contracts/test/discovery/Controller.ts
+++ packages/contracts/test/discovery/Controller.ts
@@ -4,16 +4,19 @@ import { expect } from "chai";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Controller, Controller__factory } from "../../src/types";
 
+const { parseUnits } = ethers.utils;
+
 describe("Controller", () => {
   let owner: SignerWithAddress;
   let alice: SignerWithAddress;
+  let bob: SignerWithAddress;
 
   let controller: Controller;
 
   const fixture = deployments.createFixture(async ({ deployments, ethers }) => {
     await deployments.fixture(["controller"]);
 
-    [owner, alice] = await ethers.getSigners();
+    [owner, alice, bob] = await ethers.getSigners();
 
     const controllerDeployment = await deployments.get("Controller");
 
@@ -27,5 +30,53 @@ describe("Controller", () => {
 
   describe("constructor", () => {
     it("sets the correct params", async () => {});
+  });
+
+  describe("registerProject", () => {
+    it("registers a project", async () => {
+      await controller.registerProject(
+        alice.address,
+        parseUnits("1000"),
+        parseUnits("2")
+      );
+
+      const project = await controller.getProject(0);
+
+      expect(project.id).to.eq(0);
+      expect(project.token).to.eq(alice.address);
+      expect(project.saleSupply).to.eq(parseUnits("1000"));
+      expect(project.rate).to.eq(parseUnits("2"));
+      expect(project.status).to.eq(1);
+    });
+
+    it("emits a RegisterProject event", async () => {
+      expect(
+        await controller.registerProject(
+          alice.address,
+          parseUnits("1000"),
+          parseUnits("2")
+        )
+      )
+        .to.emit(controller, "RegisterProject")
+        .withArgs(0);
+    });
+
+    it("increases the project counter", async () => {
+      await controller.registerProject(
+        alice.address,
+        parseUnits("1000"),
+        parseUnits("2")
+      );
+
+      expect(
+        await controller.registerProject(
+          bob.address,
+          parseUnits("1000"),
+          parseUnits("2")
+        )
+      )
+        .to.emit(controller, "RegisterProject")
+        .withArgs(1);
+    });
   });
 });

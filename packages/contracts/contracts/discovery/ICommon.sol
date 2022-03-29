dc0999ca68737118ecf89bf5d1c80c259a2fedda
Davide Silva <davide@finiam.com>
2022-03-29 13:15:40 +0100

    Add Batch contract interface (#57)
    Why:
    * We decided that each new batch of projects would be handled by a
      separe Batch contract
    
    How:
    * Adding an interface, `IBatch.sol`, and a bare-bones implementation of
      the `Batch` contract
    * Adding a `ICommon.sol` to share a `Period` struct between the `Batch`
      and the `Controller` contracts

---
+++ packages/contracts/contracts/discovery/ICommon.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.12;
+
+interface ICommon {
+    /// Definition of a time period
+    struct Period {
+        uint256 start;
+        uint256 end;
+    }
+}

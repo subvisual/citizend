6c629fd9825b161b9f316549f477a69344d0fbc7
Davide Silva <davide@finiam.com>
2022-04-05 14:04:01 +0100

    lint

--- packages/contracts/test/shared/utils.ts
+++ packages/contracts/test/shared/utils.ts
@@ -1,6 +1,9 @@
 import type { ContractReceipt, ContractTransaction, Event } from "ethers";
 
-export async function findEvent(tx: ContractTransaction, name: string): Promise<Event | undefined> {
+export async function findEvent(
+  tx: ContractTransaction,
+  name: string
+): Promise<Event | undefined> {
   const receipt: ContractReceipt = await tx.wait();
 
   return receipt.events?.find((e) => {

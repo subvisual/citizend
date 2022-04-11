6875130da93c1db7cf309b84ec066562284c0c39
Flavio Castro <flavio@seegno.com>
2022-04-11 10:52:53 +0100

    Add Admin page
    This page should allow the contract owner to interact with the
    contract by management the KYC list.

---
+++ packages/web/src/hooks/use-owner-action.ts
@@ -0,0 +1,51 @@
+/**
+ * Module dependencies.
+ */
+
+import { useCallback } from 'react';
+import { useContracts } from 'src/context/contracts';
+
+/**
+ * `bytes32` helper.
+ */
+
+function bytes32(value: string): string {
+  let result = `0x${value}`;
+
+  while (result.length < 64 + 2) {
+    result += '0';
+  }
+
+  return result;
+}
+
+/**
+ * Constants.
+ */
+
+export const ListId = bytes32('42');
+export const ListName = 'kyc';
+
+/**
+ * `useOwnerAction`.
+ */
+
+export function useOwnerAction() {
+  const contracts = useContracts();
+  const addToRegistry = useCallback(
+    async (address: string) => {
+      try {
+        await contracts.fractal.addUserAddress(address, ListId);
+
+        const data = await contracts.fractal.addUserToList(ListId, ListName);
+
+        return Promise.resolve(data);
+      } catch (error) {
+        // TODO: Handle error.
+      }
+    },
+    [contracts.fractal]
+  );
+
+  return { addToRegistry };
+}
---
+++ packages/web/src/pages/admin.tsx
@@ -0,0 +1,117 @@
+/**
+ * Module dependencies.
+ */
+
+import { Web3Provider } from '@ethersproject/providers';
+import { useOwnerAction } from 'src/hooks/use-owner-action';
+import { useWeb3React } from '@web3-react/core';
+import Metatags from 'src/components/core/metatags';
+import React, { useState } from 'react';
+import useWalletConnect from 'src/hooks/use-wallet-connect';
+
+/**
+ * `OwnerActions` component.
+ */
+
+function OwnerActions() {
+  const [address, setAddress] = useState('');
+  const { addToRegistry } = useOwnerAction();
+
+  return (
+    <form style={{ marginBottom: 40 }}>
+      <input
+        name={'address'}
+        onChange={event => {
+          setAddress(event.target.value);
+        }}
+        placeholder={'Wallet Address'}
+        type={'text'}
+        value={address}
+      />
+
+      <button
+        onClick={() => {
+          addToRegistry(address)
+            // eslint-disable-next-line no-console
+            .then(console.log)
+            // eslint-disable-next-line no-console
+            .catch(console.error);
+        }}
+        type={'button'}
+      >
+        {'Validate KYC'}
+      </button>
+    </form>
+  );
+}
+
+/**
+ * `Admin` page.
+ */
+
+function Admin() {
+  const { active } = useWeb3React<Web3Provider>();
+  const { onConnect, onDisconnect } = useWalletConnect();
+
+  return (
+    <>
+      <Metatags />
+
+      <a href={'/'}>{' <- Go Home'}</a>
+
+      <h1>{'Admin Page'}</h1>
+
+      {active ? (
+        <>
+          <OwnerActions />
+
+          <button onClick={onDisconnect} type={'button'}>
+            {'Disconnect'}
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            onClick={() => {
+              onConnect('metamask');
+            }}
+            type={'button'}
+          >
+            {'Metamask'}
+          </button>
+
+          <button
+            onClick={() => {
+              onConnect('walletconnect');
+            }}
+            type={'button'}
+          >
+            {'Wallet Connect'}
+          </button>
+        </>
+      )}
+    </>
+  );
+}
+
+/**
+ * Export `getStaticProps` util.
+ */
+
+export function getStaticProps() {
+  if (process.env.NODE_ENV !== 'development') {
+    return {
+      notFound: true
+    };
+  }
+
+  return {
+    props: {}
+  };
+}
+
+/**
+ * Export `Admin` page.
+ */
+
+export default Admin;

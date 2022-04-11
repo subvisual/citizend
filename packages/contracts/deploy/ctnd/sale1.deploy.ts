e4b598c42619510ee0b6d9a414c2062eeda73576
Miguel Palhas <mpalhas@gmail.com>
2022-04-11 11:15:40 +0100

    Acala deploys (#77)
    * Setting up deploy scripts for Acala

--- packages/contracts/deploy/ctnd/sale1.deploy.ts
+++ packages/contracts/deploy/ctnd/sale1.deploy.ts
@@ -1,5 +1,7 @@
+import type { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
-import { DeployFunction } from "hardhat-deploy/types";
+
+import { acalaDeploy } from "../../src/acala";
 
 const { parseUnits } = ethers.utils;
 
@@ -7,14 +9,14 @@ import { getNetworkConfig } from "../../src/deployConfigs";
 
 const func: DeployFunction = async function (hre) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy, get } = hre.deployments;
+  const { get } = hre.deployments;
 
   const aUSD = await get("aUSD");
   const registry = await get("FractalRegistry");
 
   const { ctndSale1 } = await getNetworkConfig();
 
-  await deploy("Sale1", {
+  await acalaDeploy(hre, "Sale1", {
     contract: "Sale",
     log: true,
     from: deployer,
---
+++ packages/contracts/src/acala/index.ts
@@ -0,0 +1,42 @@
+import { calcEthereumTransactionParams } from "@acala-network/eth-providers";
+import { ethers, network } from "hardhat";
+
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
+import type { DeployOptions } from "hardhat-deploy/types";
+
+export async function isAcala() {
+  return [595].includes(network.config.chainId!);
+}
+
+export async function acalaDeploy(
+  hre: HardhatRuntimeEnvironment,
+  name: string,
+  opts: DeployOptions
+) {
+  const { deploy } = hre.deployments;
+
+  return deploy(name, { ...opts, ...(await acalaDeployParams()) });
+}
+
+export async function acalaDeployParams() {
+  if (!isAcala) {
+    return {};
+  }
+
+  const txFeePerGas = "199999946752";
+  const storageByteDeposit = "100000000000000";
+  const blockNumber = await ethers.provider.getBlockNumber();
+
+  const ethParams = calcEthereumTransactionParams({
+    gasLimit: "31000000",
+    validUntil: (blockNumber + 100).toString(),
+    storageLimit: "64001",
+    txFeePerGas,
+    storageByteDeposit,
+  });
+
+  return {
+    gasPrice: ethParams.txGasPrice,
+    gasLimit: ethParams.txGasLimit,
+  };
+}

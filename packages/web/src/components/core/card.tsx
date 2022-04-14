e0a866a5e52b8b9decd4f4245a71028e506ae369
Flavio Castro <flavio@seegno.com>
2022-04-14 17:20:47 +0100

    Add Vesting countdown

--- packages/web/src/components/core/card.tsx
+++ packages/web/src/components/core/card.tsx
@@ -2,6 +2,7 @@
  * Module dependencies.
  */
 
+import { media } from 'src/styles/breakpoints';
 import styled from 'styled-components';
 
 /**
@@ -11,5 +12,9 @@ import styled from 'styled-components';
 export const Card = styled.div`
   backdrop-filter: blur(20px);
   background-color: rgba(78, 94, 124, 0.83);
-  padding: 2.5rem;
+  padding: var(--container-padding);
+
+  ${media.min.sm`
+    padding: 2.5rem;
+  `}
 `;

95fe1d231f83fb3d5315a4dd3144d51d4a2bb7e7
Miguel Palhas <mpalhas@gmail.com>
2022-05-04 10:56:15 +0100

    Revert USD currency changes

--- packages/web/src/core/constants.ts
+++ packages/web/src/core/constants.ts
@@ -16,5 +16,12 @@ export const currencyConfig = {
     decimalPlacesToDisplay: 6,
     skipTrailingZeros: true,
     symbol: undefined
+  },
+  usd: {
+    currency: 'USD',
+    decimalPlaces: 6,
+    decimalPlacesToDisplay: 2,
+    skipTrailingZeros: false,
+    symbol: '$'
   }
 } as const;

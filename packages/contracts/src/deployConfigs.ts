e2a0fe39a627b084910a106d8ac452fc4c8d9352
Zamith <luis@zamith.pt>
2022-05-06 18:28:24 +0100

    New mandala deploy

--- packages/contracts/src/deployConfigs.ts
+++ packages/contracts/src/deployConfigs.ts
@@ -60,7 +60,7 @@ async function networkConfigs(chainId: number): Promise<Config> {
         1,
         12
       );
-      const now = Math.floor(new Date(2022, 4, 6, 18, 15).getTime() / 1000);
+      const now = Math.floor(new Date(2022, 4, 6, 17, 30).getTime() / 1000);
       // const now = Math.floor(date.getTime() / 1000);
       const thirtyMinutes = 30 * 60;
       const oneDay = 60 * 60 * 24;

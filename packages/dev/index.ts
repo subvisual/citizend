bbad65d56d28296978bae5562f9811ba07a5614d
Miguel Palhas <mpalhas@gmail.com>
2022-05-11 11:12:39 +0100

    code review

--- packages/dev/index.ts
+++ packages/dev/index.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import chokidar from "chokidar";
 import debounce from "lodash.debounce";
 
 const FROM = "../contracts/deployments/localhost";
@@ -8,7 +9,8 @@ const callback = debounce((eventType: fs.PathLike) => {
   console.log("Updating web/contracts/localhost");
   fs.rmSync(TO, { recursive: true });
   fs.cpSync(FROM, TO, { recursive: true });
-  console.log(eventType);
 }, 200);
 
-fs.watch(FROM, callback);
+const watcher = chokidar.watch(`${FROM}/*.json`, { persistent: true });
+
+watcher.on("add", callback).on("change", callback);

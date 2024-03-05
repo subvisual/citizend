c38f49924f9566309bdb65729e4c7fd33ccca58c
Davide Silva <davide@subvisual.co>
2024-03-05 14:12:26 +0000

    run prettier

--- packages/contracts/wagmi.config.ts
+++ packages/contracts/wagmi.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from '@wagmi/cli'
+import { defineConfig } from "@wagmi/cli";
 import { foundry } from "@wagmi/cli/plugins";
 
 export default defineConfig({
-  out: '../web/wagmi.generated.ts',
+  out: "../web/wagmi.generated.ts",
   contracts: [],
   plugins: [
     foundry({
       exclude: [
         // We have a MockERC20.sol contract that is conflicting with the MockERC20 contract from forge
         // Until we refactor, we need to ignore it for the wagmi compilation to succeed
-        'MockERC20.sol',
+        "MockERC20.sol",
       ],
       project: "./",
       deployments: {
@@ -20,7 +20,6 @@ export default defineConfig({
           31337: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
         },
       },
-    })
+    }),
   ],
-
-})
+});

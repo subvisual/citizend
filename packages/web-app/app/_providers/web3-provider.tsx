bac223b6a14d472d2918f376ffe63f0a0765c0a1
Luís Torres <luistorres1792@gmail.com>
2024-03-08 11:52:46 +0000

    customize web3 modal theme

--- packages/web-app/app/_providers/web3-provider.tsx
+++ packages/web-app/app/_providers/web3-provider.tsx
@@ -1,25 +1,47 @@
 'use client';
 
 import { WagmiProvider } from 'wagmi';
-import { foundry, mainnet } from 'wagmi/chains';
-import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { foundry, mainnet, sepolia } from 'wagmi/chains';
+import {
+  RainbowKitProvider,
+  getDefaultConfig,
+  lightTheme,
+  type Theme,
+} from '@rainbow-me/rainbowkit';
 import { TChildren } from '@/app/_types';
+import { sora } from '../_ui/fonts';
+import merge from 'lodash.merge';
 
 const config = getDefaultConfig({
-  appName: 'Boilerplate App',
+  appName: 'Citizend',
   projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
   chains: [
     mainnet,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [foundry] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+      ? [foundry, sepolia]
+      : []),
   ],
   ssr: true,
   pollingInterval: 500,
 });
 
+const customTheme = merge(
+  lightTheme({
+    overlayBlur: 'small',
+    accentColor: '#3F69F5',
+    accentColorForeground: '#FFF',
+  }),
+  {
+    fonts: {
+      body: sora.style.fontFamily,
+    },
+  },
+) as Theme;
+
 export function Web3Provider({ children }: TChildren) {
   return (
     <WagmiProvider config={config}>
-      <RainbowKitProvider showRecentTransactions={true}>
+      <RainbowKitProvider showRecentTransactions={true} theme={customTheme}>
         {children}
       </RainbowKitProvider>
     </WagmiProvider>

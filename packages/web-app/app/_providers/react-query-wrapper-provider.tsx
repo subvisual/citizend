1b821daeb26b1d9d2d0a94d5e541de3c27eaea49
Luís Torres <luistorres1792@gmail.com>
2024-03-18 15:14:52 +0000

    Update react-query config

--- packages/web-app/app/_providers/react-query-wrapper-provider.tsx
+++ packages/web-app/app/_providers/react-query-wrapper-provider.tsx
@@ -1,14 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TChildren } from '@/app/_types';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    },
+  },
+});
 
-export function ReactQueryProviderWrapper({
-  children,
-}: TChildren) {
+export function ReactQueryProviderWrapper({ children }: TChildren) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }

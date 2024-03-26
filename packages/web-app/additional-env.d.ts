2171a6a6449fb11bbd9b38b6639e9a3f7bb3c0de
Luís Torres <luistorres1792@gmail.com>
2024-03-26 17:12:59 +0000

    fix types

--- packages/web-app/additional-env.d.ts
+++ packages/web-app/additional-env.d.ts
@@ -7,9 +7,9 @@ declare global {
       NEXT_PUBLIC_PROFILE_PROVIDER_BASE: string;
       NEXT_PUBLIC_IDOS_NODE_URL: string;
       NEXT_PUBLIC_IDOS_DB_ID: string;
-      NEXT_PUBLIC_IDOS_CONTRACT_ADDRESS: string;
+      NEXT_PUBLIC_IDOS_CONTRACT_ADDRESS: `0x${string}`;
       NEXT_PUBLIC_IDOS_CHAIN_ID: string;
-      NEXT_GRANTS_CONTRACT_ADDRESS: string;
+      NEXT_GRANTS_CONTRACT_ADDRESS: `0x${string}`;
       NEXT_ENCRYPTION_SECRET_KEY: string;
       NEXT_EVM_GRANTEE_PRIVATE_KEY: string;
     }

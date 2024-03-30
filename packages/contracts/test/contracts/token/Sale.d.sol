e54f3d9e35e6d11c270f110ac09f857df23dff89
Davide Silva <davide@subvisual.co>
2024-03-30 17:29:12 +0000

    Remove Fractal Registry from Sale and add minContribution
    Why:
    * We are using idOS instead for KYC verification and we no longer need
      the Fractal Registry contract in the Sale
    * We want to have a minimum contribution value for the token buy and be
      able to update it
    
    How:
    * Removing the `registry` ocurrences from the `Sale` contract
    * Adding a `Sale.d.sol` test to test the `Sale` contrat using Foundry
    * Updating the `MockERC20.sol` contract to no longer mint tokens for the
      deployer
    * Updating the `DevDeploy.s.sol` script to deploy the Sale contract

---
+++ packages/contracts/test/contracts/token/Sale.d.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import {Sale} from "../../../contracts/token/Sale.sol";
+import {MockERC20} from "../../../contracts/test/MockERC20.sol";
+
+contract SaleTest is Test {
+    Sale sale;
+    MockERC20 paymentToken;
+    uint256 start;
+    uint256 end;
+
+    address owner = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address alice = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address bob = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+
+    event Purchase(
+        address indexed from,
+        uint256 paymentTokenAmount,
+        uint256 tokenAmount
+    );
+
+    function setUp() public {
+        vm.startPrank(owner);
+
+        start = vm.getBlockTimestamp();
+        end = start + 60 * 60 * 24;
+
+        paymentToken = new MockERC20("USDC", "USDC", 18);
+        sale = new Sale(address(paymentToken), 1 ** 18, start, end, 100);
+
+        vm.stopPrank();
+
+        vm.startPrank(alice);
+
+        paymentToken.mint(alice, 100 ether);
+        paymentToken.approve(address(sale), 100 ether);
+
+        vm.stopPrank();
+    }
+
+    function testConstructor() public {
+        require(sale.paymentToken() == address(paymentToken));
+        require(sale.rate() == 1);
+        require(sale.start() == start);
+        require(sale.end() == end);
+        require(sale.hasRole(sale.DEFAULT_ADMIN_ROLE(), owner));
+        require(sale.hasRole(sale.CAP_VALIDATOR_ROLE(), owner));
+    }
+
+    function testBuy() public {
+        vm.startPrank(alice);
+        vm.expectEmit();
+
+        emit Purchase(address(alice), 1, 1 ether);
+
+        sale.buy(1 ether);
+
+        require(sale.risingTide_totalAllocatedUncapped() == 1 ether);
+
+        vm.stopPrank();
+    }
+
+    function testBuyBelowMinimum() public {
+        vm.prank(owner);
+
+        sale.setMinContribution(2 ether);
+
+        vm.startPrank(alice);
+
+        vm.expectRevert(bytes("can't be below minimum"));
+        sale.buy(1 ether);
+
+        sale.buy(2 ether);
+
+        require(sale.risingTide_totalAllocatedUncapped() == 2 ether);
+
+        vm.stopPrank();
+    }
+}

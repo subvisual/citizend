01ba40902bca93a949f382b16b0e45ec8b7852f2
Davide Silva <davide@subvisual.co>
2024-04-08 11:08:57 +0100

    lint

--- packages/contracts/script/Deploy.s.sol
+++ packages/contracts/script/Deploy.s.sol
@@ -7,15 +7,23 @@ import {MockERC20} from "contracts/test/MockERC20.sol";
 import {Sale} from "contracts/token/Sale.sol";
 
 contract DeployScript is Script {
- function run() public {
-   vm.startBroadcast();
+    function run() public {
+        vm.startBroadcast();
 
-   uint start = vm.unixTime();
-   uint end = start + 60 * 60 * 24 * 7;
+        uint start = vm.unixTime();
+        uint end = start + 60 * 60 * 24 * 7;
 
-   MockERC20 token = new MockERC20("USDC", "USDC", 18);
-   Sale sale = new Sale(address(token), 1 ** 18, start, end, 1000, 1000000, 2000000);
+        MockERC20 token = new MockERC20("USDC", "USDC", 18);
+        Sale sale = new Sale(
+            address(token),
+            1 ** 18,
+            start,
+            end,
+            1000,
+            1000000,
+            2000000
+        );
 
-   vm.stopBroadcast();
- }
+        vm.stopBroadcast();
+    }
 }

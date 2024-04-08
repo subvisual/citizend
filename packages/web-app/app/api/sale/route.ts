a91545cd8e948bad5e0d774c4575dc6b0d187650
Davide Silva <davide@subvisual.co>
2024-04-08 17:31:07 +0100

    code review

--- packages/web-app/app/api/sale/route.ts
+++ packages/web-app/app/api/sale/route.ts
@@ -1,5 +1,15 @@
 import { saleDetails } from '@/app/_server/sales';
 
+declare global {
+  interface BigInt {
+    toJSON: () => string;
+  }
+}
+
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
 export async function GET(_request: Request) {
   const details = await saleDetails();
 

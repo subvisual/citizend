582b8174ebd67b41d686e5ce3456f05d618b42f8
Luís Torres <luistorres1792@gmail.com>
2024-04-19 15:10:11 +0100

    fix padding

--- packages/web-app/app/my-projects/page.tsx
+++ packages/web-app/app/my-projects/page.tsx
@@ -3,7 +3,7 @@ import { KycApprovedCheck } from '../_ui/my-projects/kyc-approved-check';
 
 export default function MyProjectsPage() {
   return (
-    <main className="mb-56">
+    <main className="mb-48 md:mb-96">
       <h2>My Projects</h2>
       <KycApprovedCheck>
         <MyProjects />

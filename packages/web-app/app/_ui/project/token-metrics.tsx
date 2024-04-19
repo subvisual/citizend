6ca93d6d3143add25ac0cb529d8d06f2606b27ec
Luís Torres <luistorres1792@gmail.com>
2024-04-19 16:39:29 +0100

    Add raise progress bar

--- packages/web-app/app/_ui/project/token-metrics.tsx
+++ packages/web-app/app/_ui/project/token-metrics.tsx
@@ -9,6 +9,63 @@ type TTokenMetricsProps = {
   maxContribution: bigint;
 };
 
+const ProgressBar = ({
+  title,
+  max,
+  value,
+}: {
+  title: string;
+  max: bigint;
+  value: bigint;
+}) => {
+  // REVIEW and optimize this code
+  const valueInMillions = (BigInt(value) / BigInt(1000000n)).toString();
+  const halfInMillions = (
+    BigInt(max) /
+    BigInt(2) /
+    BigInt(1000000n)
+  ).toString();
+  const maxInMillions = (BigInt(max) / BigInt(1000000n)).toString();
+  const division = parseFloat(valueInMillions) / parseFloat(maxInMillions);
+  const percentage = `${Number(division) * 100}%`;
+
+  return (
+    <div>
+      <div className="flex flex-col px-6">
+        <label
+          htmlFor="progress-bar"
+          className="self-center text-base font-normal normal-case text-mono-800"
+        >
+          {title}
+        </label>
+        <div
+          id="progress-bar"
+          className="mt-4 flex h-6 w-full overflow-hidden rounded-md bg-blue-100"
+          role="progressbar"
+          aria-valuenow={Number(valueInMillions)}
+          aria-valuemin={0}
+          aria-valuemax={Number(maxInMillions)}
+        >
+          <div
+            className="flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-md bg-blue-500 text-center text-xs text-white transition duration-500 dark:bg-blue-500"
+            style={{ width: percentage }}
+          />
+        </div>
+        <div className="relative z-0 flex">
+          <div className="absolute left-1/2 top-0 flex flex-col  items-start">
+            <div className="h-2 w-0.5 border-[1px] border-blue-500" />
+            <div className="-translate-x-1/2">{halfInMillions}M</div>
+          </div>
+          <div className="absolute right-0 top-0 flex flex-col  items-end ">
+            <div className="border-[1px h-2 w-0.5" />
+            {maxInMillions + 'M'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const TokenMetrics = ({
   minTarget,
   maxTarget,
@@ -28,6 +85,11 @@ export const TokenMetrics = ({
       <h4 className="border-b border-mono-200 px-8 py-6 font-medium uppercase">
         Token Metrics
       </h4>
+      {process.env.NEXT_PUBLIC_APPLY_OPEN === 'false' ? (
+        <div className="m-8">
+          <ProgressBar title="Raise status" max={maxTarget} value={1000000n} />
+        </div>
+      ) : null}
       <div className="flex flex-col gap-4 p-8">
         <div className="flex flex-col gap-2 md:flex-row md:justify-between">
           <span className="text-mono-800">Target Raise:</span>

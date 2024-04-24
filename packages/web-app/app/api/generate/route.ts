f72c1d824a9b17d9b8df6e2cd969cd80fdb9a04c
Davide Silva <davide@subvisual.co>
2024-04-24 14:39:39 +0100

    comment generate endpoint

--- packages/web-app/app/api/generate/route.ts
+++ packages/web-app/app/api/generate/route.ts
@@ -1,15 +1,13 @@
-import { generateMerkleRoot } from '@/app/_server/projects/generate-merkle-root';
-
-export const dynamic = 'force-static';
-
 export async function GET(_request: Request) {
-  const root = await generateMerkleRoot();
+  // const root = await generateMerkleRoot();
+  //
+  // if (typeof root === 'object' && 'error' in root) {
+  //   return new Response(`Generate root error: ${root.error}`, {
+  //     status: 500,
+  //   });
+  // }
+  //
+  // return Response.json(root);
 
-  if (typeof root === 'object' && 'error' in root) {
-    return new Response(`Generate root error: ${root.error}`, {
-      status: 500,
-    });
-  }
-
-  return Response.json(root);
+  return new Response('root');
 }

0c169cb42e6b9a1256c959be17f4dd7664d87052
Luís Torres <luistorres1792@gmail.com>
2024-04-24 14:27:54 +0100

    fix: add error handling

--- packages/web-app/app/api/generate/route.ts
+++ packages/web-app/app/api/generate/route.ts
@@ -4,5 +4,12 @@ export const dynamic = 'force-static';
 
 export async function GET(_request: Request) {
   const root = await generateMerkleRoot();
+
+  if (typeof root === 'object' && 'error' in root) {
+    return new Response(`Generate root error: ${root.error}`, {
+      status: 500,
+    });
+  }
+
   return Response.json(root);
 }

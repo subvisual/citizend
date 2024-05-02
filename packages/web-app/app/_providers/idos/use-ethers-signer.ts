b2133fdc089bb644605aebfb045281566955c192
Luís Torres <luistorres1792@gmail.com>
2024-05-02 14:22:00 +0100

    Fix idOS linting issue

--- packages/web-app/app/_providers/idos/use-ethers-signer.ts
+++ packages/web-app/app/_providers/idos/use-ethers-signer.ts
@@ -1,14 +1,19 @@
 import { BrowserProvider, JsonRpcSigner } from 'ethers';
-import { useEffect, useMemo, useState } from 'react';
-import type { Account, Chain, Client, Transport } from 'viem';
-import { type Config, useConnectorClient } from 'wagmi';
+import { useEffect, useState } from 'react';
+import { WalletClient } from 'viem';
+import { useWalletClient } from 'wagmi';
 
-export function clientToSigner(client: Client<Transport, Chain, Account>) {
-  const { account, chain, transport } = client;
+function walletClientToSigner(walletClient: WalletClient) {
+  const { account, chain, transport } = walletClient;
+  if (!account || !chain || !transport) {
+    console.log('Missing account, chain or transport, on walletClientToSigner');
+    return undefined;
+  }
+
   const network = {
     chainId: chain.id,
     name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
+    ensAddress: chain?.contracts?.ensRegistry?.address,
   };
   const provider = new BrowserProvider(transport, network);
   const signer = new JsonRpcSigner(provider, account.address);
@@ -17,13 +22,13 @@ export function clientToSigner(client: Client<Transport, Chain, Account>) {
 
 // /** Hook to convert a viem Wallet Client to an ethers.js Signer. */
 export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
-  const { data: client } = useConnectorClient<Config>({ chainId });
+  const { data: walletClient } = useWalletClient({ chainId });
   const [signer, setSigner] = useState<JsonRpcSigner | undefined>();
 
   useEffect(() => {
     const loadClient = async () => {
-      if (client) {
-        const signerResult = await clientToSigner(client);
+      if (walletClient) {
+        const signerResult = await walletClientToSigner(walletClient);
         setSigner(signerResult);
       }
     };
@@ -32,7 +37,7 @@ export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
     return () => {
       setSigner(undefined);
     };
-  }, [client]);
+  }, [walletClient]);
 
   return signer;
 }

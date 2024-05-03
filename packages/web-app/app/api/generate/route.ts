a6a0ea78c572ad560b8dfdacfb56beed89a13043
Davide Silva <davide@subvisual.co>
2024-05-03 10:07:18 +0100

    Server route to generate Merkle Root (#234)
    Why:
    * So we can call this to get a merkle root whenever we need to, for
    testing purposes
    
    How:
    * Adding `const revalidate = 0` to prevent NextJS cache
    * Adding the needed env vars to the Github Actions workflow

--- packages/web-app/app/api/generate/route.ts
+++ packages/web-app/app/api/generate/route.ts
@@ -1,13 +1,15 @@
-export async function GET(_request: Request) {
-  // const root = await generateMerkleRoot();
-  //
-  // if (typeof root === 'object' && 'error' in root) {
-  //   return new Response(`Generate root error: ${root.error}`, {
-  //     status: 500,
-  //   });
-  // }
-  //
-  // return Response.json(root);
+import { generateMerkleRoot } from "@/app/_server/projects/generate-merkle-root";
 
-  return new Response('root');
+export const revalidate = 0;
+
+export async function GET(_request: Request) {
+  const root = await generateMerkleRoot();
+
+  if (typeof root === 'object' && 'error' in root) {
+    return new Response(`Generate root error: ${root.error}`, {
+      status: 500,
+    });
+  }
+
+  return Response.json(root);
 }

c62b949e7bc02e999a880bb1466f09cace0b9226
Luís Torres <luistorres1792@gmail.com>
2024-05-03 10:56:26 +0100

    Update data fields

--- packages/web-app/app/_ui/project/contribution/DataFields.tsx
+++ packages/web-app/app/_ui/project/contribution/DataFields.tsx
@@ -5,11 +5,12 @@ import {
 import { formatEther } from 'viem';
 import { number } from '../../utils/intl-formaters/number';
 import { useCtzndMinContributionUsdc } from '@/app/_lib/queries';
+import { usdValue } from '../../utils/intl-formaters/usd-value';
 
 const useMaxParticipants = () => {
   const { data: maxTarget, isLoading: targetLoading } =
     useReadCtzndSaleMaxTarget();
-  const min = useReadCtzndSaleTotalTokensForSale();
+  const min = useCtzndMinContributionUsdc();
   const targetValue = maxTarget ? Number(formatEther(maxTarget)) : undefined;
   const minValue = min ? Number(min) : undefined;
 
@@ -30,22 +31,31 @@ export const DataFields = () => {
 
   return (
     <div className="flex flex-col gap-6 border-t border-mono-200 p-4 md:p-6">
-      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+      <div className="grid grid-cols-1 gap-x-2 gap-y-1 md:grid-cols-2">
         <span className="text-mono-800">Current price/Token (FDV):</span>
         <div className="md:text-end">
           <span>$0.20</span>
           <span className="text-mono-800">($20m)</span>
         </div>
+        <span className="text-sm text-mono-800">Price range (FDV range):</span>
+        <div className="text-sm md:text-end">
+          <span>$0.2 - $0.4 </span>
+          <span className="text-mono-800">($20m - $40m)</span>
+        </div>
       </div>
-      <div className="flex flex-col gap-2 md:flex-row md:justify-between">
+      <div className="grid grid-cols-1 gap-x-2 gap-y-1 md:grid-cols-2">
         <span className="text-mono-800">Tokens distributed:</span>
-        <span className="md:text-end">
+        <div className="md:text-end">
           {totalTokensForSale !== undefined ? (
             <>{number(Number(formatEther(totalTokensForSale)))} CTND</>
           ) : (
             <LoadingField />
           )}
-        </span>
+        </div>
+        <span className="text-sm text-mono-800">(% of total supply):</span>
+        <div className="text-sm md:text-end">
+          <span className="text-mono-800">(2.5%)</span>
+        </div>
       </div>
       <div className="flex flex-col gap-2 md:flex-row md:justify-between">
         <span className="text-mono-800">Min. contribution amount:</span>

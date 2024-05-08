bfb118e04b7a65cf4caecde7dd7ecfa4c3cc65ea
Luís Torres <luistorres1792@gmail.com>
2024-05-09 00:47:08 +0100

    visual fixes

--- packages/web-app/app/_ui/project/project-header.tsx
+++ packages/web-app/app/_ui/project/project-header.tsx
@@ -12,20 +12,21 @@ const CountDown = () => {
   if (!saleEnd) return null;
 
   return (
-    <div className="flex h-[88px]">
+    <div className="flex md:h-[88px]">
       <svg
         width="24"
         height="88"
         viewBox="0 0 24 88"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        className="hidden md:block"
       >
         <path
           d="M1.76176 15.5025L18.5996 1.1903C19.5036 0.421901 20.6514 0 21.8379 0H24V88H5C2.23858 88 0 85.7614 0 83V19.3122C0 17.8454 0.644114 16.4525 1.76176 15.5025Z"
           fill="#1A2330"
         />
       </svg>
-      <div className="flex items-baseline bg-mono-900 font-medium text-mono-50 md:p-6">
+      <div className="flex items-baseline bg-mono-900 p-6 font-medium text-mono-50">
         <div className="flex flex-wrap content-center items-center justify-center gap-6 leading-10 text-mono-50">
           <div className="-mb-6 flex w-full justify-center self-center md:mb-0 md:w-auto">
             ENDS IN
@@ -56,6 +57,7 @@ const CountDown = () => {
         viewBox="0 0 24 88"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        className="hidden md:block"
       >
         <path
           d="M22.2382 72.4975L5.40036 86.8097C4.49635 87.5781 3.34856 88 2.16211 88H0V0H19C21.7614 0 24 2.23858 24 5V68.6878C24 70.1546 23.3559 71.5475 22.2382 72.4975Z"

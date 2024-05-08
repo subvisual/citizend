d281b498de0af4000d3b8cf7e1d86892f8d459c7
Davide Silva <davide@subvisual.co>
2024-05-08 16:39:55 +0100

    Setup AppSignal for error tracking (#248)
    Why:
    * We need to have error logging and performance tracking
    
    How:
    * Setting up AppSignal by following the configuration docs for NextJS

---
+++ packages/web-app/appsignal.cjs
@@ -0,0 +1,10 @@
+const { Appsignal } = require("@appsignal/nodejs");
+
+new Appsignal({
+  active: true,
+  name: "citizend",
+  pushApiKey: process.env.NEXT_APP_SIGNAL,
+  disableDefaultInstrumentations: [
+    "@opentelemetry/instrumentation-http",
+  ],
+});

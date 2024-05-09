2e4e88420c42755dd4b4440832fa7cf2d0ff174d
Luís Torres <luistorres1792@gmail.com>
2024-05-09 16:00:56 +0100

    fix idOSConfig

--- packages/web-app/app/_providers/idos/config.ts
+++ packages/web-app/app/_providers/idos/config.ts
@@ -1,14 +1,15 @@
-import { mainnet, sepolia } from 'viem/chains';
+import { sepolia } from 'viem/chains';
 
 export const idOsConfig = {
   container: '#idos',
   // usePasskeys: window.localStorage.getItem("use"), <-- Check how to use later
   nodeUrl: process.env.NEXT_PUBLIC_IDOS_NODE_URL,
-  evmGrantsOptions: {
-    contractAddress: process.env.NEXT_PUBLIC_IDOS_CONTRACT_ADDRESS_MAINNET,
-    chainId:
-      process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-        ? `${sepolia.id}`
-        : `${mainnet.id}`,
-  },
+  evmGrantsOptions:
+    process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+      ? {
+          contractAddress:
+            process.env.NEXT_PUBLIC_IDOS_CONTRACT_ADDRESS_MAINNET,
+          chainId: `${sepolia.id}`,
+        }
+      : undefined,
 };

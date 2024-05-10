cf85ad9f03a7a08b4e4ba755c9225e89a39c7603
Davide Silva <davide@subvisual.co>
2024-05-10 11:21:32 +0100

    Add APPSIGNAL env
    Why:
    * To distinguish between the different environments
    
    How:
    * Adding the `NEXT_APP_SIGNAL_ENV` env var

--- packages/web-app/appsignal.cjs
+++ packages/web-app/appsignal.cjs
@@ -3,6 +3,7 @@ const { Appsignal } = require('@appsignal/nodejs');
 new Appsignal({
   active: true,
   name: 'citizend',
+  environment: process.env.NEXT_APP_SIGNAL_ENV,
   pushApiKey: process.env.NEXT_APP_SIGNAL,
   disableDefaultInstrumentations: ['@opentelemetry/instrumentation-http'],
 });

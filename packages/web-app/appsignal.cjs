ffff497029e07b3b240d4a9c188bd24b209fb956
Davide Silva <davide@subvisual.co>
2024-05-10 11:26:55 +0100

    code review

--- packages/web-app/appsignal.cjs
+++ packages/web-app/appsignal.cjs
@@ -3,7 +3,7 @@ const { Appsignal } = require('@appsignal/nodejs');
 new Appsignal({
   active: true,
   name: 'citizend',
-  environment: process.env.NEXT_APP_SIGNAL_ENV,
+  environment: process.env.NEXT_APP_SIGNAL_ENV || process.env.NODE_ENV,
   pushApiKey: process.env.NEXT_APP_SIGNAL,
   disableDefaultInstrumentations: ['@opentelemetry/instrumentation-http'],
 });

bcc20a486733d582ad031eb94dc7bab278a46a0c
Luís Torres <luistorres1792@gmail.com>
2024-05-17 10:22:36 +0100

    Update app signal FE variables

--- packages/web-app/app/app-signal.ts
+++ packages/web-app/app/app-signal.ts
@@ -1,6 +1,6 @@
 import Appsignal from '@appsignal/javascript';
 
 export const appSignal = new Appsignal({
-  key: process.env.NEXT_APP_SIGNAL,
-  namespace: process.env.NEXT_APP_SIGNAL_ENV || process.env.NODE_ENV,
+  key: process.env.NEXT_PUBLIC_APP_SIGNAL,
+  namespace: process.env.NEXT_PUBLIC_APP_SIGNAL_ENV || process.env.NODE_ENV,
 });

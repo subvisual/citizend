9a545e37b972086323d5e94a17b1713ca8e03594
Luís Torres <luistorres1792@gmail.com>
2024-05-20 11:42:20 +0100

    revert api change

--- packages/web-app/app/api/generate/route.ts
+++ packages/web-app/app/api/generate/route.ts
@@ -3,9 +3,9 @@ import { generateMerkleRoot } from '@/app/_server/projects/generate-merkle-root'
 export const revalidate = 0;
 
 export async function GET(_request: Request) {
-  // if (process.env.NEXT_PUBLIC_ENABLE_TESTNETS !== 'true') {
-  //   return new Response('Not found', { status: 404 });
-  // }
+  if (process.env.NEXT_PUBLIC_ENABLE_TESTNETS !== 'true') {
+    return new Response('Not found', { status: 404 });
+  }
 
   const root = await generateMerkleRoot();
 

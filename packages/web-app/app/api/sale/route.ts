0453918b7e2790c52b2a2374d440eec600c3056c
Davide Silva <davide@subvisual.co>
2024-06-07 13:04:33 +0100

    hotfix: update api projects

--- packages/web-app/app/api/sale/route.ts
+++ packages/web-app/app/api/sale/route.ts
@@ -14,51 +14,52 @@ BigInt.prototype.toJSON = function () {
 export const dynamic = 'force-static';
 
 export async function GET(_request: Request) {
-  const details = await saleDetails();
-
-  if (typeof details === 'object' && 'error' in details) {
-    return new Response(`Sales details error: ${details.error}`, {
-      status: 500,
-    });
-  }
-
-  const tempDetails = details.map((project) => {
-    return {
-      ...project,
-      minTarget: formatUnits(project.minTarget, 6),
-      maxTarget: formatUnits(project.maxTarget, 6),
-    };
-  });
-
-  const host = process.env.NEXT_PUBLIC_DAPP_HOST;
-
-  const extendedWithTBA = [
-    ...tempDetails,
-    {
-      address: 'TBA',
-      publicKey: 'TBA',
-      project: 'The Posemesh (AUKI)',
-      description:
-        'The DePIN for AI Perception. The posemesh is a decentralized machine perception network for the next 100 billion people, devices and AI.',
-      status: 'upcoming',
-      rate: 'TBA',
-      minTarget: 'TBA',
-      maxTarget: 'TBA',
-      start: 'TBA',
-      end: 'TBA',
-      minContribution: 'TBA',
-      maxContribution: 'TBA',
-      totalTokensForSale: 'TBA',
-      startRegistration: 'TBA',
-      endRegistration: 'TBA',
-      url: 'TBA',
-      logo: `${host}/auki-logo.png`,
-      background: `${host}/auki-card-background.png`,
-      backgroundMobile: `${host}/auki-card-background.png`,
-      supplyPercentage: 'TBA',
-      currentPrice: 'TBA',
-    },
-  ];
-
-  return Response.json(extendedWithTBA);
+  return Response.json([]);
+  // const details = await saleDetails();
+  //
+  // if (typeof details === 'object' && 'error' in details) {
+  //   return new Response(`Sales details error: ${details.error}`, {
+  //     status: 500,
+  //   });
+  // }
+  //
+  // const tempDetails = details.map((project) => {
+  //   return {
+  //     ...project,
+  //     minTarget: formatUnits(project.minTarget, 6),
+  //     maxTarget: formatUnits(project.maxTarget, 6),
+  //   };
+  // });
+  //
+  // const host = process.env.NEXT_PUBLIC_DAPP_HOST;
+  //
+  // const extendedWithTBA = [
+  //   ...tempDetails,
+  //   {
+  //     address: 'TBA',
+  //     publicKey: 'TBA',
+  //     project: 'The Posemesh (AUKI)',
+  //     description:
+  //       'The DePIN for AI Perception. The posemesh is a decentralized machine perception network for the next 100 billion people, devices and AI.',
+  //     status: 'upcoming',
+  //     rate: 'TBA',
+  //     minTarget: 'TBA',
+  //     maxTarget: 'TBA',
+  //     start: 'TBA',
+  //     end: 'TBA',
+  //     minContribution: 'TBA',
+  //     maxContribution: 'TBA',
+  //     totalTokensForSale: 'TBA',
+  //     startRegistration: 'TBA',
+  //     endRegistration: 'TBA',
+  //     url: 'TBA',
+  //     logo: `${host}/auki-logo.png`,
+  //     background: `${host}/auki-card-background.png`,
+  //     backgroundMobile: `${host}/auki-card-background.png`,
+  //     supplyPercentage: 'TBA',
+  //     currentPrice: 'TBA',
+  //   },
+  // ];
+  //
+  // return Response.json(extendedWithTBA);
 }
